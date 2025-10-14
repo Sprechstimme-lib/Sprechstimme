d56da51a08de123108d704990fdd3dff8a524dc2
breatn <andante_chantry.6f@icloud.com>
2025-10-14 18:56:51 +0100

    Update __init__.py

--- sprechstimme/__init__.py
+++ sprechstimme/__init__.py
@@ -1,9 +1,10 @@
 from .core import new, create, play, get, midi_to_freq, note_to_freq
 from .track import Track
+from .song import Song
 from . import waves, filters
 
 __all__ = [
     "new", "create", "play", "get",
-    "Track", "waves", "filters",
+    "Track", "Song", "waves", "filters",
     "midi_to_freq", "note_to_freq"
 ]
